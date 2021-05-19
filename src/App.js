f9c559609c6b962e1a0f58c77b6dee56bc52550e
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-05-19 21:20:07 +0200

    create a web chat with messageFeed and messages components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
+import {ChatEngine} from 'react-chat-engine'
 import './App.css';
+import ChatFeed from './components/ChatFeed'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID="102306df-2b9c-46d4-8d7c-60bea7a7bf63"
+      userName="simo-nkosi"
+      userSecret="Willies1992"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
